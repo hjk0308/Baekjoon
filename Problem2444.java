f1e8e7b1474d4e3b82a0436529bf42c85dbbfa87
hjk0308 <80231108+hjk0308@users.noreply.github.com>
2023-04-12 12:04:29 +0900

    Add files via upload

---
+++ Problem2444.java
@@ -0,0 +1,47 @@
+package github;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem2444 {
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 입력받는 단계
+		int N = Integer.parseInt(br.readLine());  
+		
+		// 문제에 조건에 맞게 별 찍는 단계
+		for(int i = 1; i <= N; i++)
+		{
+			for(int j = 1; j <= N-i; j++)
+				bw.write(" ");
+			
+			for(int k = 1; k <= 2*i-1; k++)
+				bw.write("*");
+			
+			bw.newLine();
+		}
+		
+		for(int i = N-1; i >= 1; i--)
+		{
+			for(int j = 1; j <= N-i; j++)
+				bw.write(" ");
+			
+			for(int k = 1; k <= 2*i-1; k++)
+				bw.write("*");
+			
+			bw.newLine();
+		}
+		
+		// 출력하는 단계
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+}
