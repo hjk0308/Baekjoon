bbff92617c02d522191191907c90a86cd916f22b
hjk0308 <80231108+hjk0308@users.noreply.github.com>
2023-04-19 15:05:05 +0900

    Add files via upload

---
+++ Problem2522.java
@@ -0,0 +1,55 @@
+package github;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem2522 {
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// 입력받는 단계
+		int N = Integer.parseInt(br.readLine());
+		
+		// 규칙에 맞게 별을 찍는 단계
+		for(int i = 1; i <= N; i++)
+		{
+			for(int j = 1; j <= N-i; j++)
+			{
+				bw.write(" ");
+			}
+			
+			for(int j = 1; j <= i; j++)
+			{
+				bw.write("*");
+			}
+			
+			bw.newLine();
+		}
+		
+		for(int i = N-1; i >= 1; i--)
+		{
+			for(int j = 1; j <= N-i; j++)
+			{
+				bw.write(" ");
+			}
+			
+			for(int j = 1; j <= i; j++)
+			{
+				bw.write("*");
+			}
+			
+			bw.newLine();
+		}
+		
+		// 출력하는 단계	
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+}
